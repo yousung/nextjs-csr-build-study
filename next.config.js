68d8adb67a92fd8ca4ad1d40c69e2d384cbadb4a
안유성 <nug22kr@gmail.com>
2023-10-05 22:37:30 +0900

    github action

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  output: "export",
+  assetPrefix: "/nextjs-csr-build-study",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
